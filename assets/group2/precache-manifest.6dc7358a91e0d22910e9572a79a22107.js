0cce80a4029105d06b78e07917a58da52c3f0595
David Tucker <david@globomantics.com>
2020-04-09 14:17:30 -0400

    Updating assets for project

---
+++ assets/group2/precache-manifest.6dc7358a91e0d22910e9572a79a22107.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d9960eebad48b115a95a3a6dffabca4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02e3767811155db702e7",
+    "url": "/static/css/main.68ce6a07.chunk.css"
+  },
+  {
+    "revision": "285c69d595d9ef1490fc",
+    "url": "/static/js/2.0d9af1a6.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/static/js/2.0d9af1a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02e3767811155db702e7",
+    "url": "/static/js/main.5a82ada5.chunk.js"
+  },
+  {
+    "revision": "20ccc8393ebe1f74ba4c",
+    "url": "/static/js/runtime-main.9ecb0d16.js"
+  }
+]);
