e7d3800b20a59a0779815a74ffa6becbcee5d198
David Tucker <david@globomantics.com>
2020-04-24 19:37:08 -0400

    Updating tests

--- test/module2.test.js
+++ test/module2.test.js
@@ -3,9 +3,30 @@ const expect = require('chai').expect;
 chai.use(require('chai-http'));
 chai.use(require('chai-string'));
 
-const host = "https://" + bucketName + ".s3.amazonaws.com";
-const webhost = process.env.PS_S3_WEBHOST_URL;
+const addTrainingSlashIfNotPresent = (input) => {
+  let output = input;
+  var lastChar = input.substr(-1);
+  if (lastChar !== '/') {
+    output = output + '/';
+  }
+  return output;
+}
+
+const getHostURL = () => {
+  if(!process.env.PS_S3_BUCKET_NAME || process.env.PS_S3_BUCKET_NAME.length < 5) {
+    return '';
+  }
+  let host = 'https://' + process.env.PS_S3_BUCKET_NAME + '.s3.amazonaws.com';
+  var lastChar = host.substr(-1);
+  if (lastChar != '/') {
+    host = host + '/';
+  }
+  return addTrainingSlashIfNotPresent(host);
+};
+
 const bucketName = process.env.PS_S3_BUCKET_NAME;
+const host = getHostURL();
+const webhost = addTrainingSlashIfNotPresent(process.env.PS_S3_WEBHOST_URL);
 const accountID = process.env.PS_AWS_ACCOUNT_ID;
 
 describe('Deploy a static site on Amazon S3', () => {
@@ -27,30 +48,31 @@ describe('Deploy a static site on Amazon S3', () => {
     expect(bucketName, 'Bucket name should not contain and periods').to.not.include('.');
     expect(bucketName.length, 'Bucket name must be entered by the user').to.be.gt(3);
 
-    const res = await chai.request(host).get('/index.html');
+    const res = await chai.request(host).get('index.html');
     expect(res, 'Permissions should be set to make index.html available to the public').to.have.status(200);
     expect(res, 'File should be served from Amazon S3').to.have.header('server', 'AmazonS3');
     expect(res, 'File should be an HTML file, verify the correct file was uploaded').to.be.html;
     expect(res.text, 'File should contain the correct content, make sure the correct file was uploaded').to.have.string('ps-index');
 
-    const res2 = await chai.request(host).get('/test.txt');
+    const res2 = await chai.request(host).get('test.txt');
     expect(res2, 'Permissions should be set to make test.txt available to the public').to.have.status(200);
     expect(res2, 'File should be served from Amazon S3').to.have.header('server', 'AmazonS3');
     expect(res2, 'File should be an HTML file, verify the correct file was uploaded').to.be.text;
     expect(res2.text, 'File should contain the correct content, make sure the correct file was uploaded').to.have.string('ps-ccp-02');
+    
   });
 
   it('error page should appear if navigating to page that does not exist @verify-static-hosting', async () => {
-    expect(process.env.PS_S3_WEBHOST_URL, 'Static website hosting URL must be entered by the user').to.be.a('string');
-    expect(process.env.PS_S3_WEBHOST_URL, 'Enter in the full URL from the static website hosting configuration').to.startWith('http');
+    expect(webhost, 'Static website hosting URL must be entered by the user').to.be.a('string');
+    expect(webhost, 'Enter in the full URL from the static website hosting configuration').to.startWith('http');
 
-    const res = await chai.request(webhost).get('/file-that-doesnt-exist');
+    const res = await chai.request(webhost).get('file-that-doesnt-exist');
     expect(res, 'Make sure you did not upload any additional files into the bucket').to.have.status(404);
     expect(res, 'File should be served from Amazon S3').to.have.header('server', 'AmazonS3');
     expect(res, 'Make sure the proper error file was set to error.html in the static website hosting settings').to.be.html;
     expect(res.text, 'Make sure the proper error file was set to error.html in the static website hosting settings').to.have.string('ps-ccp-03');
 
-    const res2 = await chai.request(webhost).get('/');
+    const res2 = await chai.request(webhost).get('');
     expect(res2, 'Permissions should be set to make index.html file available and static website hosting settings have index.html as index file').to.have.status(200);
     expect(res2, 'File should be served from Amazon S3').to.have.header('server', 'AmazonS3');
     expect(res2, 'File should be an HTML file, verify the correct file was uploaded').to.be.html;
@@ -61,13 +83,13 @@ describe('Deploy a static site on Amazon S3', () => {
     expect(process.env.PS_S3_WEBHOST_URL, 'Static website hosting URL must be entered by the user').to.be.a('string');
     expect(process.env.PS_S3_WEBHOST_URL, 'Enter in the full URL from the static website hosting configuration').to.startWith('http');
 
-    const res = await chai.request(webhost).get('/verify.txt');
+    const res = await chai.request(webhost).get('verify.txt');
     expect(res, 'Make sure you added the bucket policy to make all files publicly readable').to.have.status(200);
     expect(res, 'File should be served from Amazon S3').to.have.header('server', 'AmazonS3');
     expect(res, 'Make sure the proper error file was set to error.html in the static website hosting settings').to.be.text;
     expect(res.text, 'Make sure the proper error file was set to error.html in the static website hosting settings').to.have.string('ps-ccp-03');
 
-    const res2 = await chai.request(webhost).get('/');
+    const res2 = await chai.request(webhost).get('');
     expect(res2, 'Make sure you added the bucket policy to make all files publicly readable and that you uploaded all files in group2').to.have.status(200);
     expect(res2, 'File should be served from Amazon S3').to.have.header('server', 'AmazonS3');
     expect(res2, 'File should be an HTML file, verify the correct file was uploaded').to.be.html;
